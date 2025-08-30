399b12419d3deb5e59eeb882e3dd139a6c84bbcb
Konstantina Papafragkaki <papafragakikonstantina@gmail.com>
2025-08-30 20:25:41 +0300

    Changed Makefile and standalone to test the new code for llm partitions.

--- scripts/calculate_avg_time.py
+++ scripts/calculate_avg_time.py
@@ -32,7 +32,7 @@ def parse_times(filename, model_name, num_tokens, num_partitions, runs, occurren
         lines = file.readlines()
 
     for i in reversed(range(len(lines))):
-        if f"Model: {model_name} for {num_tokens} tokens" in lines[i]:
+        if f"Model: {model_name}" in lines[i]: #for {num_tokens} tokens
             if count == model_match_index:
                 if lines[i+minimum_stats+1] == "\n":
                     if "Inference time to run a model:" not in lines[i+1]:
